6b95f1a46e9c2a1146662ca892288e9deeb4ee51
Vishal Swaroop <vswaroop22@gmail.com>
2019-12-05 11:46:26 +0530

    my second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,24 +8,34 @@ import { HomeComponent } from './home/home.component';
 import { BlogViewComponent } from './blog-view/blog-view.component';
 import { BlogCreateComponent } from './blog-create/blog-create.component';
 import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    AboutComponent,
     BlogViewComponent,
     BlogCreateComponent,
-    BlogEditComponent
+    BlogEditComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
     
-      {path:'home',component:HomeComponent}
-      
-      
+      {path:'home',component:HomeComponent},
+      {path:'',redirectTo:'home',pathMatch:'full'},
+      {path:'about',component:AboutComponent},
+      {path: 'blog/:blogId',component:BlogViewComponent},
+      {path: 'create',component:BlogCreateComponent},
+      {path: 'edit/:blogId', component:BlogEditComponent},
+      {path: ' " " ',component:NotFoundComponent}
+    
+    
 
-    ])
+      ])
 
   ],
   providers: [],
